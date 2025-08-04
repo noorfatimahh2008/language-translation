c7973864918805241a206868dee5820c0ee62ec6
Noor Fatima <noorfatimahh2008@gmail.com>
2025-08-04 20:02:38 +0500

    Create encoder.py

---
+++ models/encoder.py
@@ -0,0 +1,17 @@
+import torch.nn as nn
+import torch
+
+class EncoderRNN(nn.Module):
+    def __init__(self, input_size, hidden_size):
+        super().__init__()
+        self.hidden_size = hidden_size
+        self.embedding = nn.Embedding(input_size, hidden_size)
+        self.gru = nn.GRU(hidden_size, hidden_size)
+
+    def forward(self, input, hidden):
+        embedded = self.embedding(input).view(1, 1, -1)
+        output, hidden = self.gru(embedded, hidden)
+        return output, hidden
+
+    def initHidden(self):
+        return torch.zeros(1, 1, self.hidden_size)
